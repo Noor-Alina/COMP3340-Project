122e97093607b8d670fdc92fd6ce40b3f97bc8ba
nandinipatel <patelnandini26@gmail.com>
2021-07-11 19:53:56 -0400

    setting up routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Home from './dynamic/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './dynamic/Home';
+import Signup from './static/Signup';
+import Signin from './static/Signin';
+import About from './static/About';
+import Contact from './static/Contact';
+import TAC from './static/TAC';
+import FAQ from './static/FAQ';
+
+//TAC: Terms and Conditions page
+//FAQ: Frequently Asked Questions page
 
 function App() {
   return (
       <Router>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/Signup" component={Signup} />
+            <Route exact path="/Signin" component={Signin} />
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Contact" component={Contact} />
+            <Route exact path="/TAC" component={TAC} /> 
+            <Route exact path="/FAQ" component={FAQ} /> 
           </Switch>
       </Router>
   );
