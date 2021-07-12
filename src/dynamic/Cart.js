451ad1443aab0864f7969d4f85f4d0e92b67a352
nandinipatel <patelnandini26@gmail.com>
2021-07-12 01:25:34 -0400

    home page changes

---
+++ src/dynamic/Cart.js
@@ -0,0 +1,13 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import React from 'react';
+
+export default class Cart extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
