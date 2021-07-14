9f07b5f6c3cae5133cfa43b6046a5c162c7ead74
ali20-0 <ali20_0@hotmail.com>
2021-07-13 20:04:33 -0400

    edited text

--- src/static/About.js
+++ src/static/About.js
@@ -9,7 +9,7 @@ export default class About extends React.Component{
         <div>
             <Header/>
             <br/>
-            <div style="background-color:#f7f7f7;padding:15px;">
+            <div>
                 <h1>About Us</h1>
   
             </div>
@@ -19,13 +19,13 @@ export default class About extends React.Component{
       
                 <div class="main">
                     <h2>About EcoShopper</h2>
-                    <p>EcoShopper is one of the best online stores that specializes in selling ethically sourced products.</p>
+                    <p>EcoShopper is an online stores that specializes in selling ethically sourced products.</p>
                     <img src="assets\images\sun.jpg" style="width:100%" />
                 </div>
 
                 <div class="right">
                     <h2>Our Goal?</h2>
-                    <p>At EcoShopper, customers are our number one priority, and our goal is to make sure the products they are getting are absolutely ethically sourced.</p>
+                    <p>At EcoShopper, customers are our number one priority, and our goal is to make sure that they only get ethically sourced products.</p>
                     <h2>Where?</h2>
                     <p>Our store is online based with the intention to expand in the future.</p>
                     <h2>Prices?</h2>
