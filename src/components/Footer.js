f8388fbe60eb8daf1e632b900550d87fe42fb864
dhwaniPatel <dhwanipatel1911@gmail.com>
2021-07-14 13:48:29 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends React.Component {
                             <h5>Get to Know Us</h5>
                             <ul class = "footer-links">
                                 <li>About us</li>
-                                <li>Contact Us</li>
+                                <li>Contact us</li>
                                 <li>Frequently Asked Questions (FAQs)</li>
                             </ul>
                         </div>
