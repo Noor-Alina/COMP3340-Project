2f3d49abfe20d1c8cda981a549b1c98c92ed00bb
dhwaniPatel <dhwanipatel1911@gmail.com>
2021-07-14 10:35:20 -0400

    changes added

--- src/static/Contact.js
+++ src/static/Contact.js
@@ -1,11 +1,31 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 export default class Contact extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {name: '',email: '',message: ''}
+        this.state = {fname: '', lname: '',email: '',message: ''}
+      }
+      handleSubmit(e) {
+        e.preventDefault();
+      
+        axios({
+          method: "POST",
+          url:"http://localhost:3002/send",
+          data:  this.state
+        }).then((response)=>{
+          if (response.data.status === 'success') {
+            alert("Message Sent.");
+            this.resetForm()
+          } else if(response.data.status === 'fail') {
+            alert("Message failed to send.")
+          }
+        })
+      }
+      resetForm(){
+        this.setState({fname: '',lname:'', email: '', message: ''})
       }
 
     render() {  
