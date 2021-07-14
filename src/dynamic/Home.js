467c3b729423967e56f73c7edf58541d7c71d0c2
nandinipatel <patelnandini26@gmail.com>
2021-07-13 20:26:18 -0400

    home changes

--- src/dynamic/Home.js
+++ src/dynamic/Home.js
@@ -2,21 +2,26 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import React from 'react';
 
-export default class Home extends React.Component{
-      
-    render() {
+function Home() {
         return (
         <div>
             <Header/>
             <br/>
-                <form class="search-bar">
-                    <input type="text" placeholder="Search..." name="search-button" />
-                    <button type="submit">Submit</button>
-                </form>
-                <img src= "../assets/images/canada-flag.png" />
+                <div class = "row">
+                    <div class = "col-md-8">
+                        <form class="search-bar">
+                            <input type="text" placeholder="Search..." name="search-button" />
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                    <div class = "col-md-2">
+                        <div classname="flag" alt="country selector"></div>
+                    </div>
+                </div>
             <br/>
             <Footer/>
         </div>
         );
-    }
 }
+
+export default Home;
