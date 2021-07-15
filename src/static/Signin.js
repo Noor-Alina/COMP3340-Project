a9b25971f92fa52fe8b2643397bdc2ecd2a159f7
shbhat789 <sh.bhat789@gmail.com>
2021-07-15 08:20:31 -0400

    Message: - Created a generic account on firebase and Added FireBase Authetication Key to the project - Created sign up page and made sure user gets created on firebase - Created login page and made sure user is authenticated before   proceeding for further access - Added some more react modules as a helper components for my features - Added history component to keep track of browsing history - Instead of having header and footer in every component, added that to   APP.js so that it stays at one place and is visible everywhere.

---
+++ src/static/Signin.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {withRouter} from 'react-router';
+
+import firebase from '../components/Firebase';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Signin extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      email : '',
+      password : '',
+      errorMessage : null
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (e) {
+    const itemName = e.target.name;
+    const itemValue = e.target.value;
+    this.setState ({
+      [itemName] : itemValue });
+  }
+
+  handleSubmit (e) {
+    var registrationInfo = {
+      email : this.state.email,
+      password: this.state.password
+    }
+    e.preventDefault();
+    firebase.auth().signInWithEmailAndPassword(
+      registrationInfo.email,
+      registrationInfo.password
+    ).then (() => {
+        this.props.history.push("/");
+    })
+    .catch( error => {
+      if (error.message !== null){
+        this.setState({ errorMessage : error.message  })
+      } else {
+        this.setState({ errorMessage : null })
+      }
+    });
+  }
+
+  renderError () {
+    return (
+        <div className="col-12 alert alert-danger px-3 ">
+          {this.state.errorMessage}
+        </div>
+    );
+  }
+
+  render () {
+    return (
+        <div> 
+            <form className="mt-3" onSubmit={this.handleSubmit}>
+                    <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-6">
+                        <div className="card bg-light">
+                            <div className="card-body">
+                            <h3 className="font-weight-light mb-3">Log in</h3>
+                            <section className="form-group">
+                            { this.state.errorMessage && this.renderError }
+                                <label
+                                className="form-control-label sr-only"
+                                htmlFor="Email">
+                                Email
+                                </label>
+                                <input
+                                required
+                                className="form-control"
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                                />
+                            </section>
+                            <section className="form-group">
+                                <input
+                                required
+                                className="form-control"
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                                />
+                            </section>
+                            <div className="form-group text-right mb-0">
+                                <button className="btn btn-success" type="submit">
+                                Log in
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+            </form>
+        </div>
+    );
+  }
+}
+
+export default withRouter(Signin)
---
+++ src/static/TAC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class TAC extends React.Component{
+      
+    render() {
+        return (
+        <div>
+            Terms and Conditions
+        </div>
+        );
+    }
+}
