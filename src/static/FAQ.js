186c0a3e34280e01c5b6d65bd6161fd3b6fa7be7
AlinaNoor <An10_23@hotmail.com>
2021-07-14 23:55:41 -0400

    Faq landing page created

---
+++ src/static/FAQ.js
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import React from 'react';
+
+export default class About extends React.Component{
+      
+    render() {
+        return (
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+        );
+    }
+}
