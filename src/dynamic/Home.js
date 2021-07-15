39b5f1c0cfb759db8e3ea6e92df7720806af30c5
nandinipatel <patelnandini26@gmail.com>
2021-07-15 16:32:37 -0400

    xyz

--- src/dynamic/Home.js
+++ src/dynamic/Home.js
@@ -1,31 +1,6 @@
-<<<<<<< HEAD
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Canada from '../assets/images/canada-flag.png'
-import React, { Component } from 'react';
-import Modal from './Modal.js';
-
-class Home extends Component {
-    constructor() {
-        super();
-        this.state = {
-          show: false
-        };
-        this.showModal = this.showModal.bind(this);
-        this.hideModal = this.hideModal.bind(this);
-      }
-    
-      showModal = () => {
-        this.setState({ show: true });
-      };
-    
-      hideModal = () => {
-        this.setState({ show: false });
-      };
-=======
 import React from 'react';
->>>>>>> 1c06305120c35489bc2d5671e868ae3d6a305883
 
+class Home extends React.Component{
     render() {
         return (
         <div>
--- src/static/About.js
+++ src/static/About.js
@@ -8,43 +8,7 @@ export default class About extends React.Component{
     render() {
         return (
         <div>
-<<<<<<< HEAD
-            
-            <Header/>
-            
-            {/* <div>
-            <div class="about">
-                <h1>About EcoShopper</h1>
-            </div>
-                
-            <br/>
-
-            <div style={{overflow: 'auto'}}>
-            
-      
-                <div class="main">
-                
-                    <p style={{textAlign: 'left', fontSize: 'large', borderStyle: 'outset'}}>EcoShopper is an online retailer of products that inform, educate, and inspire. We specializes in selling ethically sourced products. We strive to keep our customers happy. 
-                    We sincerely appreciate your support and hope that the products you purchase from EcoShopper Store bring you pleasure!</p>
-                    <img src="/images/efp.jpg" alt="Eco friendly"/>
-                </div>
-
-                <div class="right">
-                    <h2>Our Goal?</h2>
-                    <p style={{textAlign: 'left'}}>At EcoShopper, customers are our number one priority, and our goal is to make sure that they only get ethically sourced products.</p>
-                    <h2>Where?</h2>
-                    <p style={{textAlign: 'left'}}>Our store is online based with the intention to expand in the future.</p>
-                    <h2>Prices?</h2>
-                    <p style={{textAlign: 'left'}}>We try to get our valued customres the best value for their money.</p>
-                </div> 
-            </div> 
-
-            <br/>
-            <Footer/> 
-        </div> */}
-=======
             About
->>>>>>> 1c06305120c35489bc2d5671e868ae3d6a305883
         </div>
         );
 }
