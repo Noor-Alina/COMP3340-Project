2badeb89c3df8fdd232763e7b8f86a95aedcc345
Dhwani Patel <66276887+dhwanipatel14@users.noreply.github.com>
2021-07-15 14:53:27 -0400

    Update Contact.js

--- src/static/Contact.js
+++ src/static/Contact.js
@@ -1,13 +1,73 @@
-import React from 'react'
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import React from 'react';
+import axios from 'axios';
 
-export default class Contact extends React.Component{
-      
-    render() {
-        return (
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fname: '', lname: '', email: '', message: '' }
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    axios({
+      method: "POST",
+      mailto: "ecoshopper@gmail.com",
+      data: this.state
+    }).then((response) => {
+      if (response.data.status === 'success') {
+        alert("Message Sent.");
+        this.resetForm()
+      } else if (response.data.status === 'fail') {
+        alert("Message failed to send.")
+      }
+    })
+  }
+  resetForm() {
+    this.setState({ fname: '', lname: '', email: '', message: '' })
+  }
+
+  //test test
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <br />
         <div>
-            Contact
+          <h1 className="contact-h1">Contact-Us Form</h1>
         </div>
-        
-        );
-    }
+        <section>
+          <form className="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+            <label className="contact-label" for="fname" > First Name: <input className="contact-input" type="text" placeholder="First Name" required /> </label><br />
+            <label className="contact-label" for="lname" > Last Name: <input className="contact-input" type="text" placeholder="Last Name" required /> </label><br />
+            <label className="contact-label" for="email-id">Email Id: <input className="contact-input" type="email" aria-describedby="emailHelp" placeholder="Email Address" required /></label><br />
+            <label className="contact-label" for="message">Message: <br /><textarea className="contact-text" class="c-control" rows="5" cols="80" placeholder="Your Message" required></textarea> </label><br />
+            <button className="contact-button" type="submit">Submit</button>
+            <button className="contact-button" type="reset">Reset</button>
+            <div>
+              <br />
+            </div>
+          </form>
+        </section>
+        <br/>
+      </div>
+    );
+  }
+
+  onNameChange(event) {
+    this.setState({ name: event.target.value })
+  }
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value })
+  }
+
+  onMessageChange(event) {
+    this.setState({ message: event.target.value })
+  }
+
+  handleSubmit(event) {
+  }
 }
