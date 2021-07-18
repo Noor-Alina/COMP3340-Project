68be30a401ce5fbae8e1b006cb0ac91f1f700ddb
AlinaNoor <An10_23@hotmail.com>
2021-07-18 14:40:16 -0400

    Added faq blocks

--- src/static/FAQ.js
+++ src/static/FAQ.js
@@ -5,7 +5,29 @@ export default class FAQ extends React.Component{
     render() {
         return (
         <div>
-            
+            <div class="title">
+                <h1> FAQs - Orders </h1>
+            </div>
+
+    
+            <div class="row">
+                <div class="column block" onclick="location.href='Orders.html';" style="cursor: pointer;">
+                    <h2>Orders</h2>
+                    <p>Data..</p>
+                </div>
+                <div class="column block" onclick="location.href='Delivery.html';" style="cursor: pointer;">
+                    <h2>Delivery</h2>
+                    <p>Data..</p>
+                </div>
+                <div class="column block" onclick="location.href='ReturnsRefunds.html';" style="cursor: pointer;">
+                    <h2>Returns and Refunds</h2>
+                    <p>Data..</p>
+                </div>
+                <div class="column block" onclick="location.href='Account.html';" style="cursor: pointer;">
+                    <h2>Account</h2>
+                    <p>Data..</p>
+            </div>
+        </div>
         </div>
         );
     }
