f0c5fb631e74788220fd097a373247b380af1e4a
AlinaNoor <An10_23@hotmail.com>
2021-07-18 21:44:59 -0400

    creating the faq categories

--- src/static/FAQ.js
+++ src/static/FAQ.js
@@ -4,29 +4,30 @@ export default class FAQ extends React.Component{
       
     render() {
         return (
+
         <div>
             <div class="title">
-                <h1> FAQs - Orders </h1>
+                <h1> Frequently Asked Questions </h1>
             </div>
 
-    
             <div class="row">
-                <div class="column block" onclick="location.href='Orders.html';" style="cursor: pointer;">
+                <div class="column block">
                     <h2>Orders</h2>
                     <p>Data..</p>
                 </div>
-                <div class="column block" onclick="location.href='Delivery.html';" style="cursor: pointer;">
+                <div class="column block">
                     <h2>Delivery</h2>
                     <p>Data..</p>
                 </div>
-                <div class="column block" onclick="location.href='ReturnsRefunds.html';" style="cursor: pointer;">
+                <div class="column block">
                     <h2>Returns and Refunds</h2>
                     <p>Data..</p>
                 </div>
-                <div class="column block" onclick="location.href='Account.html';" style="cursor: pointer;">
+                <div class="column block">
                     <h2>Account</h2>
                     <p>Data..</p>
             </div>
+
         </div>
         </div>
         );
