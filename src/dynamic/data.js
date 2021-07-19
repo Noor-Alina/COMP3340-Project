f7f54f938f2b73deebbd460c276634dcea3c3c95
ali20-0 <ali20_0@hotmail.com>
2021-07-19 16:11:03 -0400

    cart

---
+++ src/dynamic/data.js
@@ -0,0 +1,28 @@
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: 'https://assets.vogue.com/photos/5e878d232918c10008aa3044/1:1/w_640,h_640,c_limit/slide_10.jpg',
+            title: 'T-Shirt',
+            desc: 'white tshirt',
+            price: '19.99',
+        },
+        {
+            id: 1,
+            img: 'https://cdn.shopify.com/s/files/1/1296/1875/products/little-english-white-cable-knit-sweater_1000x.jpg?v=1567623771',
+            title: 'Sweater',
+            desc: 'knitted sweater',
+            price: '49.99',
+        },
+        {
+            id: 1,
+            img: 'http://cdn.shopify.com/s/files/1/0229/1646/4718/products/11-oz-orca-mug.png?v=1581036897',
+            title: 'Coffee Mug',
+            desc: 'White Coffee Mug',
+            price: '9.99',
+        },
+    ]
+};
+
+export default data;
