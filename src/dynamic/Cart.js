ee018169ad99fb2733d9cea1a6439ad205cd3c91
ali20-0 <ali20_0@hotmail.com>
2021-07-28 15:28:23 -0400

    added cart css to index css

--- src/dynamic/Cart.js
+++ src/dynamic/Cart.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CartModel from "../components/CartModel";
-import "./Cart.css";
 import Subtotal from "./Subtotal";
 
 function Cart() {
