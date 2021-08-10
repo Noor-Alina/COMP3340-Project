8499e3f6eaa5db041250441c3633d3a229d6b29a
dhwaniPatel <dhwanipatel1911@gmail.com>
2021-08-10 15:07:23 -0400

    adding 20 countries

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,17 +19,32 @@ class Header extends React.Component {
                 <Nav>
                         <NavDropdown title="Country">
                             <NavDropdown.Item href="country/Canada">Canada</NavDropdown.Item>
-                            <NavDropdown.Item href="country/India">India</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Germany">Germany</NavDropdown.Item>
+                            <NavDropdown.Item href="country/America">America</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Africa">Africa</NavDropdown.Item>
                             <NavDropdown.Item href="country/Autralia">Australia</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Pakistan">Pakistan</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Brazil">Brazil</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Carribean">Carribean</NavDropdown.Item>
+                            <NavDropdown.Item href="country/China">China</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Europe">Europe</NavDropdown.Item>
+                            <NavDropdown.Item href="country/France">France</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Germany">Germany</NavDropdown.Item>
+                            <NavDropdown.Item href="country/HongKong">HongKong</NavDropdown.Item>
+                            <NavDropdown.Item href="country/India">India</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Italy">Italy</NavDropdown.Item>
                             <NavDropdown.Item href="country/Iran">Iran</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Syria">Syria</NavDropdown.Item>
                             <NavDropdown.Item href="country/Iraq">Iraq</NavDropdown.Item>
                             <NavDropdown.Item href="country/Japan">Japan</NavDropdown.Item>
-
-                        </NavDropdown>
+                            <NavDropdown.Item href="country/Luxembourg">Luxembourg</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Malaysia">Malaysia</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Mexico">Mexico</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Norway">Norway</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Pakistan">Pakistan</NavDropdown.Item>   
+                            <NavDropdown.Item href="country/Syria">Syria</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Quatar">Quatar</NavDropdown.Item>
+                            <NavDropdown.Item href="country/Turkey">Turkey</NavDropdown.Item>
+                            <NavDropdown.Item href="country/UK">UK</NavDropdown.Item>
                     
+                        </NavDropdown> 
                     </Nav>
                     { !user && <Link className="nav-item nav-link" to = "Signin"> Log in </Link>}
                     { !user && <Link className="nav-item nav-link" to = "Signup"> Sign up </Link> }
@@ -42,7 +57,6 @@ class Header extends React.Component {
                     { user && <Link className="nav-item nav-link" to="Signin" onClick={e => logOutUser(e)}> Log out <FaSignOutAlt />  </Link> }
                     <Link className="nav-item nav-link" to = "Cart"> 🛒︁ </Link>
                     { user && <Link className="nav-item nav-link" to="/Signin" onClick={e => logOutUser(e)}> Log out <FaSignOutAlt />  </Link> }
-                    
                 </div>
                 </div>
             </nav>
