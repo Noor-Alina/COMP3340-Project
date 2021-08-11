7a159dda9e33777681c18a23d3fd7abaa4e61267
dhwaniPatel <dhwanipatel1911@gmail.com>
2021-08-11 12:27:57 -0400

    updated dropdown-menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FaCartPlus, FaSignOutAlt, FaUser} from 'react-icons/fa'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar,Nav, NavDropdown } from 'react-bootstrap';
+import 'semantic-ui-css/semantic.min.css';
 
-class Header extends React.Component {
 
-    render() {
-        const { user, logOutUser } = this.props;
+function Header({ user, logOutUser } ) {    
+        
+        const[country, setCountry] = useState('');
+
+    const handleCountry = (event) => {
+        event.preventDefault();
+
+        setCountry(<Link>{`/${event.target.value}`}</Link>)
+    }
+
         return (
             <nav className="site-nav family-sans navbar navbar-expand bg-success navbar-dark higher">
                 <div className="container-fluid">
@@ -16,35 +24,35 @@ class Header extends React.Component {
                      
                 <Link className="navbar-brand" to = "/"> <FaCartPlus className="mr-1" /> EcoShopper </Link>
                 <div className="navbar-nav ml-auto">
-                <Nav>
-                        <NavDropdown title="Country">
-                            <NavDropdown.Item href="country/Canada">Canada</NavDropdown.Item>
-                            <NavDropdown.Item href="country/America">America</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Africa">Africa</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Australia">Australia</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Brazil">Brazil</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Carribean">Carribean</NavDropdown.Item>
-                            <NavDropdown.Item href="country/China">China</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Europe">Europe</NavDropdown.Item>
-                            <NavDropdown.Item href="country/France">France</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Germany">Germany</NavDropdown.Item>
-                            <NavDropdown.Item href="country/HongKong">HongKong</NavDropdown.Item>
-                            <NavDropdown.Item href="country/India">India</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Italy">Italy</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Iran">Iran</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Iraq">Iraq</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Japan">Japan</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Luxembourg">Luxembourg</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Malaysia">Malaysia</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Mexico">Mexico</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Norway">Norway</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Pakistan">Pakistan</NavDropdown.Item>   
-                            <NavDropdown.Item href="country/Syria">Syria</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Quatar">Quatar</NavDropdown.Item>
-                            <NavDropdown.Item href="country/Turkey">Turkey</NavDropdown.Item>
-                            <NavDropdown.Item href="country/UK">UK</NavDropdown.Item>
-                        </NavDropdown> 
-                    </Nav>
+                <select name='country' value={country} onClick={(e) => handleCountry(e)}>
+                    <option value='Canada' >Canada</option>
+                           
+                           <option  value='America'>America</option>
+                           <option value='Africa' >Africa</option>
+                           <option >Australia</option>
+                           <option >Brazil</option>
+                           <option >Carribean</option>
+                           <option >China</option>
+                           <option >Europe</option>
+                           <option >France</option>
+                           <option >Germany</option>
+                           <option >HongKong</option>
+                           <option >India</option>
+                           <option >Italy</option>
+                           <option >Iran</option>
+                           <option >Iraq</option>
+                           <option >Japan</option>
+                           <option >Luxembourg</option>
+                           <option >Malaysia</option>
+                           <option >Mexico</option>
+                           <option >Norway</option>
+                           <option >Pakistan</option>   
+                           <option >Syria</option>
+                           <option >Quatar</option>
+                           <option >Turkey</option>
+                           <option >UK</option>
+                    </select>
+
                     { !user && <Link className="nav-item nav-link" to = "Signin"> Log in </Link>}
                     { !user && <Link className="nav-item nav-link" to = "Signup"> Sign up </Link> }
                     { user && <Link className="nav-item nav-link" to = "Cart"> Cart </Link> }
@@ -60,7 +68,7 @@ class Header extends React.Component {
                 </div>
             </nav>
         )
-    }
+    
 }
 
 export default Header;
