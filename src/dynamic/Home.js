e268cb7e1ca26e706eea860a39e1d126a373671c
nandinipatel <patelnandini26@gmail.com>
2021-08-11 23:07:10 -0400

    test

--- src/dynamic/Home.js
+++ src/dynamic/Home.js
@@ -1,57 +1,76 @@
 import React from 'react';
 import axios from 'axios';
 import { Card, Button } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { increaseitemQuantity, setItemsToCart } from '../actions';
+import { useDispatch, useSelector } from 'react-redux';
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-        }
-    }
+//test test test test test
+function Home() {
+    
+    const [users, setUsers]= useState([])
+        useEffect(()=>{
+            axios.get("/products").then((response) => {
+                setUsers(response.data)
+            });
+        },[])
+   const items = useSelector((state) => state.items.cart);
 
-    componentDidMount() {
-        axios.get("/products").then((response) => {
-            this.setState({ users: response.data });
-        });
-    }
+   const dispatch= useDispatch()
 
-    render() {
-        const { users } = this.state;
-        console.log(users);
+   const addItemToCart=(user)=>{
+
+   const index = items.findIndex(
+        (cartItem) => cartItem.id === user.id
+      );
+
+      if (index >= 0) {
+          console.log(user.id)
+           dispatch(increaseitemQuantity(user.id))
+
+      } else {
+        dispatch(setItemsToCart(user))
+   }
+
+//    function getProduct(id) {
+//        console.log(id);
+//    }
+//                          <a style={{ cursor: 'pointer' }} onClick={()=>getProduct(user.id)}> 
 
         return (
         <div>
-            <div className = "row">
-                    <div className = "col-md-8">
-                        <form className="search-bar">
-                            <input type="text" placeholder="Search..." name="search-button" />
-                            <button type="submit">Submit</button>
-                        </form>
-                    </div>
-                </div>
-                <br></br>
+              
+            <div style={{marginTop: 20}} className="r-flex align-items-center justify-content-center margin-top:5">
+                <br/>
+                    <form className="search-bar">
+                        <input type="text" placeholder="search..." name="search-button" />
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                <br/>
+            </div>
+                <br /><br />
+
             <ul className="flex-container wrap">
                 {users.map(user =>
                 <ol className="flex-item">
-                    <Card style={{ width: '18rem', marginBottom: '2rem'}}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>{user.name}</Card.Title>
-                                <Card.Text>
-                                    {user.email}
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Add to Cart</Button>
-                            </Card.Body>
-                    </Card>
+                        <Card style={{ width: '18rem', marginBottom: '2rem'}}>
+                            <Card.Img variant="top" />
+                                <Card.Body>
+                                    <Card.Title>{user.name}</Card.Title>
+                                    <Card.Text>
+                                        {user.email}
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                <div> $ {user.price}</div><br />
+                                    <Button onClick={()=>{addItemToCart(user)}} variant="primary">Add to Cart</Button>
+                                </Card.Body>
+                        </Card>
                 </ol>
                 )}
             </ul>                
         </div>
         );
-    }
 }
-
-export default Home;
+}
